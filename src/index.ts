9d0fca4ed112f23bbc66b1c7575cccb7c8d859f1
Ben Meyrick <bameyrick@gmail.com>
2021-05-12 12:58:37 +0100

    #8 Handle decimal arguments (#9)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@
 export function numberArrayFromRange(min: number, max: number): number[] {
   const result: number[] = [];
 
-  for (let i = min; i <= max; i++) {
+  if (min > max) {
+    throw new Error('Min is greater than max');
+  }
+
+  for (let i = min > 0 ? Math.floor(min) : Math.ceil(min), l = max > 0 ? Math.floor(max) : Math.ceil(max); i <= l; i++) {
     result.push(i);
   }
 
